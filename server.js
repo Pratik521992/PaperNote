341c82f1c6e55333eef563118845616fa255022f
pratik521992 <pratik521992@gmail.com>
2018-12-20 00:44:25 +0530

    chages

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const jwt = require('jsonwebtoken')
 const  mongodb = require('mongodb');
 const objectId  = require('mongodb').ObjectId;
 const bodyparser = require('body-parser');
 const app = express();
-const port = 5002;
+const port = 5004;
 const cors = require('cors');
 
 app.use(bodyparser.json()); 
@@ -25,10 +26,25 @@ async function loadData(){
   return client.db('papernote_main').collection('entries');
 }
 
-app.get('/api/db', async(req, res) => {
+app.get('/api/db' , async(req, res) => {
+
   const entries = await loadData();
   res.send(await entries.find({}).toArray()); 
 })
+app.post('/api/loginSecure', verifyToken, (req, res) => {
+
+  jwt.verify(req.token, 'shhhh', (err, authData) => {
+    if(err) {
+     
+      res.sendStatus(403);
+    }
+    else {
+      res.send({
+        authData
+      })
+    }
+  }) 
+}) 
 
 app.post('/api/db/post', async(req, res) =>{
   console.log(req.body.id)
@@ -62,93 +78,35 @@ app.delete('/api/db/del/:id', async(req, res) => {
   res.status(200).send();
 })
 
-app.get('/api/store', (req, res) =>{
+app.post('/api/db/login', (req, res)=>{
+  //mock user
+  const user = {
+    id:1,
+    user: 'Pratik',
+    pass: 'Nini'
+  }
 
-    const store = [{
-        id: 'overhead-press',
-        title: 'Axis Laser 3D Printer',
-        description: 'Axis 3D laser printer: a new desktop tool that uses laser cutter/engraver technology to shape wood, leather, fabric & more at an amazing price.',
-        muscles: 'Technology',
-        Dollar: 23
-      },
-      {
-        id: 'dips',
-        title: 'Portable Finger Lock',
-        description: 'PORTABLE FINGER LOCK : It is a mini portable smart box lock, and its pocket size can be carried to everywhere you want. Powerful smart universal fingerprint lock.',
-        muscles: 'Technology',
-        Dollar: 7
-      },
-      {
-        id: 'barbell-curls',
-        title: 'QRobot Sweeper',
-        description: 'The cube gives a signal which QRobot uses to position itself in the room. It will know where exactly in the room it is and which part of the floor it has mopped or swept',
-        muscles: 'Technology',
-        Dollar: '0'
-      },
-      {
-        id: 'f1',
-        title: 'Sustainable Premium Sneakers',
-        description: 'Handmade unisex sneakers designed to inspire change towards a more conscious lifestyle & equitable future.',
-        muscles: 'Fashion',
-        Dollar: '0'
-      },
-    {
-        id: 'f2',
-        title: 'Designer Swim Shorts For Men',
-        description: 'Luxury Designer Swim Shorts For Men. Tailored for a superior fit. Made with water resistant, quick drying fabric. Go from Pool to Party.',
-        muscles: 'Fashion',
-        Dollar: '0'
-      },
-    {
-        id: 'f3',
-        title: 'Cue Hardcover Backpack',
-        description: 'The Cue Hardcover Backpack is a polycarbonate bag for daily + long-distance travel and Made of durable polyester; Ideal for hiking and camping.',
-        muscles: 'Fashion',
-        Dollar: '0'
-      },
-    {
-        id: 'd1',
-        title: 'Robotic Camera Assistant',
-        description: 'Replace your fluid head with a 4-axis motorized head. Hold up to 15lbs and capture amazing video and time-lapse on a tripod or slider.',
-        muscles: 'Design',
-        Dollar: '0'
-      },
-    {
-        id: 'd2',
-        title: 'SmartCup',
-        description: 'Experience pleasure in every sip of the perfectly prepared beverage with SmartCup automated brewing technology.',
-        muscles: 'Design',
-        Dollar: '0'
-      },
-    {
-        id: 'd3',
-        title: 'Delta Printer : Photograghy',
-        description: 'Capture and print, handy and mobile.Updated for the digital age yet retaining the simplicity and fun.',
-        muscles: 'Design',
-        Dollar: '0'
-      },
-    {
-        id: 'ff1',
-        title: 'Cricket Protein Powder',
-        description: 'The superfood of the future for folks whose current protein powder is bad for the environment and for themselves.',
-        muscles: 'Food',
-        Dollar: '0'
-      },
-    {
-        id: 'ff2',
-        title: 'Sea Lunch Box',
-        description: 'An eco-friendly, portion separated lunch box witha stainless steel, easy-clean, flip top design.',
-        muscles: 'Food',
-        Dollar: '0'
-      },
-    {
-        id: 'ff3',
-        title: 'Vegan Chocolate',
-        description: 'Heavenly Chocolate that tastes divine & is free from dairy, gluten, soy and palm oil & only 4% sugar.',
-        muscles: 'Food',
-        Dollar: '0'
-      },
-    ]
-    res.send(store);
+  jwt.sign({user}, 'shhhh' ,(err, token)=>{
+      res.json({token})
+  })
+
+
+  
 })
+
+function verifyToken(req, res, next){
+
+  const bearerHeader = req.headers['authorization'];
+  if(typeof bearerHeader !== 'undefined'){
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    console.log(bearer)
+    next();
+
+  }else{
+    res.sendStatus(403);
+  }
+}
+
 app.listen(port, () => console.log(`server started at ${port}`) )
